ad9c535a56b60111ee9353b5a467f5d9a980cf9a
Dellear <358009775@qq.com>
2021-08-22 12:20:00 +0800

    适配上游调整

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,19 +15,15 @@ echo
 
 echo -e "========================2. 检测配置文件========================\n"
 
-if [ -s /config/crontab.list ]
-then
-  echo -e "检测到config配置目录下存在crontab.list，自动导入定时任务...\n"
-  crontab /config/crontab.list
-  echo -e "成功添加定时任务...\n"
-else
-  echo -e "检测到config配置目录下不存在crontab.list或存在但文件为空，请手动添加crontab.list\n"
-  echo
+if [ ! -s /config/crontab.list ]; then
+  echo -e "检测到config配置目录下不存在crontab.list，从脚本中复制一份用于初始化...\n"
+  cp -fv ${SCRIPT_DIR}/config/crontab.list /config/crontab.list
+  echo -e "成功添加默认定时任务...\n"
 fi
 
 if [ ! -s /config/config.json ]; then
   echo -e "检测到config配置目录下不存在config.json，从脚本中复制一份用于初始化...\n"
-  cp -fv ${SCRIPT_DIR}/config.json /config/config.json
+  cp -fv ${SCRIPT_DIR}/config/config.json /config/config.json
   echo
 fi
 
